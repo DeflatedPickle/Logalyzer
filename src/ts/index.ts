167314945eb9a2c3815b3d7c74071cad06144708
DeflatedPickle <DeflatedPickle@gmail.com>
2019-11-02 00:17:59 +0000

    Made the RegEx pattern and formatter, as well as the table columns customizable

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,36 +1,53 @@
 // Libraries
-const Tabulator = require('tabulator-tables');
+const tabulator = require('tabulator-tables');
+const pascalcase = require('pascalcase');
 
+// JSON
+const regex = require("../json/regex.json");
 // Stylesheets
 const style = require("../scss/index.scss");
 
 // TODO: Allow columns to be defined on the page
-const table = new Tabulator("#table", {
+const table = new tabulator("#table", {
     height: 200,
     data: [],
     layout: "fitColumns",
-    columns: [
-        {
-            title: "Time",
-            field: "time"
-        },
-        {
-            title: "Thread",
-            field: "thread"
-        },
-        {
-            title: "Level",
-            field: "level"
-        },
-        {
-            title: "Class",
-            field: "class"
-        },
-        {
-            title: "Information",
-            field: "info"
+    columns: []
+});
+emptyTable(table, true);
+
+document.getElementById('re-select').addEventListener('change', (e) => {
+    // @ts-ignore
+    const dict = regex[e.target.value];
+
+    (<HTMLInputElement>document.getElementById('re-pattern')).value = dict["pattern"];
+    (<HTMLInputElement>document.getElementById('re-formatter')).value = dict["formatter"];
+    (<HTMLInputElement>document.getElementById('tb-columns')).value = dict["columns"];
+
+    emptyTable(table, true);
+});
+
+function emptyTable(table: Tabulator, newColumn: boolean = false) {
+    for (const column of table.getColumns()) {
+        // @ts-ignore
+        column.delete();
+    }
+
+    if (newColumn) {
+        table.addColumn({title: "Please, Tabulate Me", field: ""});
+    }
+}
+
+document.getElementById('tabulate').addEventListener('click', () => {
+    const text = (<HTMLInputElement>document.getElementById('tb-columns')).value;
+
+    if (text != null) {
+        emptyTable(table);
+
+        for (let col of text.split(",")) {
+            table.addColumn({title: pascalcase(col), field: col})
         }
-    ]
+    }
 });
 
 document.getElementById('table').addEventListener('dragover', (e) => {
@@ -45,13 +62,11 @@ document.getElementById('table').addEventListener("drop", (e) => {
     e.stopPropagation();
     e.preventDefault();
 
-    // TODO: Add an input to reformat this
-    const regExFormatter = /([\[\/\]:])/g;
+    const regEx = new RegExp((<HTMLInputElement>document.getElementById('re-pattern')).value, 'g');
 
-    // TODO: Add an input to define the columns
-    const regEx = /\[([0-9]+:[0-9]+:[0-9]+)] \[([a-zA-Z]+)\/([A-Z]+)[\]]?( \[[A-Za-z]+]: |: ):?(.*)/g;
+    const regExFormatter = new RegExp((<HTMLInputElement>document.getElementById('re-formatter')).value, 'g');
 
-    const columns = ['time', 'thread', 'level', 'class', 'info'];
+    const columns = (<HTMLInputElement>document.getElementById('tb-columns')).value.split(",");
 
     const fileList = e.dataTransfer.files;
 
@@ -63,10 +78,11 @@ document.getElementById('table').addEventListener("drop", (e) => {
                 let data = [];
 
                 for (let [index, line] of reader.result.split("\r\n").slice(0, 8).entries()) {
+                    console.log(line);
                     document.getElementById("text").append(line, document.createElement("br"));
 
                     let dataEntry = {id: index};
-                    
+
                     let match;
                     do {
                         match = regEx.exec(line);
