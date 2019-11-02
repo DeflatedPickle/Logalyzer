898056b3769e8623883f28c05fa8acc3430d3359
DeflatedPickle <DeflatedPickle@gmail.com>
2019-11-02 23:38:52 +0000

    Disabled file loading if there is no pattern

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -9,12 +9,14 @@ require("../coffee/index.coffee");
 // Stylesheets
 require("../scss/index.scss");
 
+const holder = document.getElementById("holder");
+holder.innerText = "Feed Me File.";
 const table = new tabulator("#table", {
     height: 400,
     data: [],
     layout: "fitData",
     columns: [],
-    placeholder: "Feed Me File.",
+    placeholder: holder,
 });
 
 function emptyTable(table: Tabulator, newColumn: boolean = false) {
@@ -42,72 +44,100 @@ document.getElementById('re-select').addEventListener('change', (e) => {
     emptyTable(table, true);
 });
 
-document.getElementById('table').addEventListener('dragover', (e) => {
+function checkInserts(e: DragEvent) {
     e.stopPropagation();
     e.preventDefault();
 
-    e.dataTransfer.dropEffect = 'copy';
+    const pattern = (<HTMLInputElement>document.getElementById('re-pattern')).value;
+
+    if (pattern.length > 0) {
+        e.dataTransfer.dropEffect = 'copy';
+    }
+    else {
+        holder.innerText = "Add A RegEx Pattern First!";
+        e.dataTransfer.effectAllowed = "none";
+        e.dataTransfer.dropEffect = "none";
+    }
+}
+
+document.getElementById('table').addEventListener('dragover', (e) => {
+    checkInserts(e);
 });
 
+document.getElementById("table").addEventListener("dragenter", (e)=> {
+    checkInserts(e);
+});
+
+document.getElementById("table").addEventListener("dragleave", (e) => {
+    checkInserts(e);
+});
 
 document.getElementById('table').addEventListener("drop", (e) => {
     e.stopPropagation();
     e.preventDefault();
 
-    const regEx = new RegExp((<HTMLInputElement>document.getElementById('re-pattern')).value, 'g');
+    const pattern = (<HTMLInputElement>document.getElementById('re-pattern')).value;
+
+    const regEx = new RegExp(pattern, 'g');
     const regExFormatter = new RegExp((<HTMLInputElement>document.getElementById('re-formatter')).value, 'g');
     const columns = (<HTMLInputElement>document.getElementById('tb-columns')).value.split(",");
 
-    const text = (<HTMLInputElement>document.getElementById('tb-columns')).value;
+    if (pattern.length > 0) {
+        const text = (<HTMLInputElement>document.getElementById('tb-columns')).value;
 
-    if (text != null) {
-        emptyTable(table);
+        if (text != null) {
+            emptyTable(table);
 
-        for (let col of text.split(",")) {
-            table.addColumn({title: pascalcase(col), field: col})
+            for (let col of text.split(",")) {
+                table.addColumn({title: pascalcase(col), field: col})
+            }
+        }
+
+        const fileList = e.dataTransfer.files;
+
+        for (let file of fileList) {
+            const reader = new FileReader();
+
+            reader.addEventListener("loadend", () => {
+                if (typeof reader.result === "string") {
+                    // @ts-ignore
+                    document.getElementById("text").value = "";
+
+                    let data = [];
+
+                    const start = +(<HTMLInputElement>document.getElementById('start-rows')).value;
+                    const limit = +(<HTMLInputElement>document.getElementById('end-rows')).value;
+
+                    for (let [index, line] of reader.result.split("\r\n").slice(start, limit).entries()) {
+                        // @ts-ignore
+                        document.getElementById("text").value += line;
+                        // document.createElement("br");
+
+                        let dataEntry = {id: index};
+
+                        let match;
+                        do {
+                            match = regEx.exec(line);
+                            if (match) {
+                                for (let [key, value] of match.slice(1).entries()) {
+                                    // @ts-ignore
+                                    dataEntry[columns[key]] = value.replace(regExFormatter, " ");
+                                }
+                            }
+                        } while (match);
+
+                        data.push(dataEntry);
+                    }
+
+                    table.replaceData(data);
+                }
+            });
+
+            reader.readAsText(file);
         }
     }
-
-    const fileList = e.dataTransfer.files;
-
-    for (let file of fileList) {
-        const reader = new FileReader();
-
-        reader.addEventListener("loadend", () => {
-            if (typeof reader.result === "string") {
-                // @ts-ignore
-                document.getElementById("text").value = "";
-
-                let data = [];
-
-                const start = +(<HTMLInputElement>document.getElementById('start-rows')).value;
-                const limit = +(<HTMLInputElement>document.getElementById('end-rows')).value;
-
-                for (let [index, line] of reader.result.split("\r\n").slice(start, limit).entries()) {
-                    // @ts-ignore
-                    document.getElementById("text").value += line;
-                    // document.createElement("br");
-
-                    let dataEntry = {id: index};
-
-                    let match;
-                    do {
-                        match = regEx.exec(line);
-                        if (match) {
-                            for (let [key, value] of match.slice(1).entries()) {
-                                // @ts-ignore
-                                dataEntry[columns[key]] = value.replace(regExFormatter, " ");
-                            }
-                        }
-                    } while (match);
-
-                    data.push(dataEntry);
-                }
-
-                table.replaceData(data);
-            }
-        });
-
-        reader.readAsText(file);
+    else {
+        e.dataTransfer.effectAllowed = "none";
+        e.dataTransfer.dropEffect = "none";
     }
 });
